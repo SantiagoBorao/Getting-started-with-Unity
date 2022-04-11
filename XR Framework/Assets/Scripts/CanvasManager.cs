e2cfe0b9d713e73e271ea0032c31c4b0fdb8ff3e
SantiagoB <santiagoborao@gmail.com>
2022-04-11 12:26:14 +0200

    Canvas es hijo de Cubo, + func para mostrar/ocultarlo

--- XR Framework/Assets/Scripts/CanvasManager.cs
+++ XR Framework/Assets/Scripts/CanvasManager.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 public class CanvasManager : MonoBehaviour
 {
 
-    public void ToggleCanvas(){
-        
-        if (gameObject.activeSelf == true)
-        {
-           gameObject.SetActive(false);
-        } else {
-           gameObject.SetActive(true);
-        }
+    public void ShowCanvas()
+    {
+        gameObject.SetActive(true);
     }
 
+    public void HideCanvas()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void ToggleCanvas()
+    {
+        if (gameObject.activeSelf == true)
+        {
+            gameObject.SetActive(false);
+        } else {
+            gameObject.SetActive(true);
+        }
+    }
 }
 
